64e7579dddcc87cfeefb216138f9711f7ac64c09
josmas <josmasflores@gmail.com>
2021-03-09 17:25:53 +0000

    allow CORS with headers in main function

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const fetch = require('node-fetch');
 
 module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   // TODO add checks for body.url
   let url = 'https://api.github.com/users/github'
   if (req.body.url) {
