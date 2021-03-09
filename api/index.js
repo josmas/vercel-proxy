7e39471dc424496b2c79f47304daf04cc587f96d
josmas <josmasflores@gmail.com>
2021-03-09 16:12:11 +0000

    use fetch to grab data

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
-module.exports = (req, res) => {
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+  // TODO add checks for body.url
+  const result = await fetch(req.body.url).catch(e => res.send(e))
+  console.log('The result is ', result)
   res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
+    result
   })
 }
