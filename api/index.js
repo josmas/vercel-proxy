9170ca16381c35c3e9d5b42ee695674e781ad2b4
josmas <josmasflores@gmail.com>
2021-03-09 16:30:52 +0000

    use fetch properly

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
 const fetch = require('node-fetch');
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   // TODO add checks for body.url
-  const result = await fetch(req.body.url).catch(e => res.send(e))
-  console.log('The result is ', result)
-  res.json({
-    result
-  })
+  fetch(req.body.url)
+    .then(res => res.json())
+    .then(json => res.json(json))
+    .catch(e => res.send(e))
 }
