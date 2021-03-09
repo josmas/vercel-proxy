80b565a64e017ff8cb7bbb25db438de0a24d1391
josmas <josmasflores@gmail.com>
2021-03-09 16:41:39 +0000

    use a default url if one is not passed

--- api/index.js
+++ api/index.js
@@ -2,7 +2,11 @@ const fetch = require('node-fetch');
 
 module.exports = (req, res) => {
   // TODO add checks for body.url
-  fetch(req.body.url)
+  let url = 'https://api.github.com/users/github'
+  if (req.body.url) {
+    url = req.body.url
+  }
+  fetch(url)
     .then(result => result.json())
     .then(json => res.send(json))
     .catch(e => res.send(e))
